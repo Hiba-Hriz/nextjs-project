b0f8de0b72a2025d4583dcee789193dcc93495ec
hiba <hiba.hriz@enis.tn>
2024-07-18 22:52:32 +0100

    Fixed build error and updated files

--- app/api/auth/[kindeAuth]/route.ts
+++ app/api/auth/[kindeAuth]/route.ts
@@ -1,15 +1,10 @@
 import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server'
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest } from 'next/server'
 
 export async function GET(
   request: NextRequest,
   { params }: any
-): Promise<Response> {
+) {
   const endpoint = params.kindeAuth
-  try {
-    const result = handleAuth(request, endpoint) as unknown as Response
-    return result
-  } catch (error) {
-    return new NextResponse('Error handling auth', { status: 500 })
-  }
+  return handleAuth(request, endpoint)
 }
