58df00ce2e837117dbe8f04df19264117f9c3f21
hiba <hiba.hriz@enis.tn>
2024-07-19 19:29:32 +0100

    final commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,11 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "MSI CONSULTANTS",
   description: "projet",
+  icons: {
+    icon: "/logo.png",
+    shortcut: "/logo.png",
+    apple: "/logo.png" 
+  },
 };
 
 export default function RootLayout({
