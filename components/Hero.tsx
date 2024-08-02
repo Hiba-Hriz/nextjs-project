d49c3c7d98b4703772aad31df8a61860ed9d0b2b
hiba <hiba.hriz@enis.tn>
2024-08-02 16:44:26 +0100

    final commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,28 +16,28 @@ const Hero = () => {
 
 
         </div>
-        <div className="h-[89vh] w-full dark:blue dark:bg-black-100  flex 
+        <div className="h-[79vh] lg:h-[89vh] w-full dark:blue dark:bg-black-100  flex 
         items-center justify-center absolute top-0 left-0">
       <div className="absolute pointer-events-none
        inset-0 flex items-center justify-center dark:bg-black-100 bg-white
         [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
       
     </div>
-    <div className='flex justify-center relative my-20
+    <div className='flex justify-center relative my-8 lg:my-20
     z-10'>
       <div className='max-w-[89vw] md:max-w-2xl 
       lg:max-w-[60vw] flex flex-col items-center
       justify-center'>
         <h2 className='uppercase tracking-widest
-        text-xs text-center   text-blue-100 max-w-80 -mt-20 md:mt-0'>
+        text-xs text-center   text-blue-100 max-w-80 -mt-24 md:mt-0'>
           projets ERP
         </h2>
         <TextGenerateEffect 
-        className='text-center text-3xl
+        className='text-center text-4xl
         md:text-5xl lg:text-6xl'
         words="L'expertise en systèmes d'informations"
         />
-        <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl text-white'>
+        <p className='text-center md:tracking-wider mb-0 text-sm md:text-lg lg:text-2xl text-white'>
           votre partenaire de référence
         </p>
         <a href='/sign-in'>
